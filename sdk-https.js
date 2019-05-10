3fde23e20cb03cd9a4b4cd32425c95106374545d
anhhoang236 <bongdem_unknow_92@yahoo.com>
2019-05-10 14:54:50 +0700

    Update sdk-https.js

--- sdk-https.js
+++ sdk-https.js
@@ -1,3 +1,4 @@
+console.log(pa_id, domain);
 var PushdyIns = new function() {
     this.pa_id = pa_id;
     this.domain = domain || "localhost";
@@ -83,8 +84,6 @@ var PushdyIns = new function() {
 
     this.init = function(){
         var hostname = location.hostname.replace(/^www\./,'');
-        console.log(hostname);
-        console.log(this.domain);
         if(hostname.indexOf(this.domain)==(hostname.length - this.domain.length)){
             if(document.readyState==='complete'){
                 PushdyIns.checkSubscription();
